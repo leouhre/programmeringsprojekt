ea77d7cf6d5d493d374bf296ea2c2e56676122e4
leouhre <leouhrejakobsen@gmail.com>
2020-06-12 12:10:04 +0200

    ADC1 working

--- Inc/ADC1.h
+++ Inc/ADC1.h
@@ -5,11 +5,13 @@
  *      Author: leo
  */
 
+#include <stdint.h>
+
 #ifndef ADC1_H_
 #define ADC1_H_
 
-void ADC1_config()
-uint16_t ADC1_1_read()
-uint16_t ADC1_2_read()
+void ADC1_config();
+uint16_t ADC1_1_read();
+uint16_t ADC1_2_read();
 
 #endif /* ADC1_H_ */
