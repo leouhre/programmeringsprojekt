d07c046be3baff3acc1be3c9cbc416e21f718524
theodorgto <66820813+theodorgto@users.noreply.github.com>
2020-06-16 10:19:07 +0200

    changed bullet

--- Src/bullet.c
+++ Src/bullet.c
@@ -1,38 +1,47 @@
 #include "bullet.h"
 #include "math.h"
 
-void bullet_init(bullet_t *bullet, spaceship_t *sh) {
-    uint8_t i, str;
+void bullet_init(bullet_t *bullet, spaceship_t sh) {
+    uint8_t i, str = 0;
     for(i = 0; i < 20; i++) { // find number of bullets in array
-        if(bullet[i] == '\0') break;
-        str ++;
+        if(bullet[i].alive == 0) break;
+        str++;
     }
+    bullet[str+1].alive = 1;
 
-    bullet[str+1]->x=sh.x;
-    bullet[str+1]->y=sh.y;
+    bullet[str+1].x = sh.x;
+    bullet[str+1].y = sh.y;
 
-    bullet[str+1]->x <<= 14;
-    bullet[str+1]->y <<= 14;
+    bullet[str+1].x <<= 14;
+    bullet[str+1].y <<= 14;
 
-    bullet[str+1]->direction=sh.direction;
-    bullet[str+1]->bullet_type = sh.bullet_type;
+    bullet[str+1].angle = sh.angle;
+    bullet[str+1].bullet_type = sh.bullet_type;
 }
 
-void bullet_update(bullet_t bullet, entities *enemies){
+void bullet_update(bullet_t *bullet, uint8_t entities){
     uint8_t i, j;
 
 
-	for (i = 0; i < 20; i++ ) {
-        bullet[i].x += bullet[i].direction.x;
-        bullet[i].y += bullet[i].direction.y;
+	for (i = 0; i < 20; i++ ) { // update all bullets
+        gotoxy(bullet[i].x >> 14, bullet[i].y >> 14);
+        printf(" "); //clear current bullet position
 
-        if(enteties[bullet[i].x][bullet[i].y] != '\0') { //if enemy is hit delete bullet
-            bullet[i] = '\0';
+        bullet[i].x += calcos(bullet[i].angle);
+        bullet[i].y += calcsin(bullet[i].angle);
+
+        gotoxy(bullet[i].x >> 14, bullet[i].y >> 14);
+
+        if(enteties[bullet[i].x >> 14][bullet[i].y >> 14] != 0) { //if enemy is hit delete bullet
+            //goto xy play animation
             for (j = i; j < 19; j++){
-                if(bullet[j] == '\0') break;
+                if(bullet[j].alive == 0) break;
                 bullet[j] = bullet[j+1];
             }
-            bullet[19] = '\0';
+            bullet[19].alive = 0;
+            i--; // go one position back in the array
+        } else {
+            printf("o");
         }
 
 	}
